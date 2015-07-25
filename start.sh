964dbc3334b117aa8a81c23d8d7bdc4c5624f6ab
Eric Ihli <ericihli@gmail.com>
2015-07-24 21:40:04 -0700

    (devops) deployment fix

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-node ./server/server.js
+if [ "$SERVER_TYPE" == "tweethandler" ]
+  then
+  node ./tweetHandler/server.js
+elif [ "$SERVER_TYPE" == "streaming" ]
+  then
+  node ./streaming/server.js
+else
+  node ./server/server.js
+fi
