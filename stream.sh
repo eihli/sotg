3299f76d14ba09af12c504a91327964d69a2206f
Eric Ihli <ericihli@gmail.com>
2015-07-25 15:01:22 -0700

    (deployment) Update startup scripts.

--- stream.sh
+++ stream.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export RDS_HOSTNAME=sotg-staging-db.ctifvcvmvtjl.us-west-2.rds.amazonaws.com
-export HANDLER_ADDRESS=ec2-52-27-78-211.us-west-2.compute.amazonaws.com
-export API_ADDRESS=sotg-staging.elasticbeanstalk.com
-export PORT=80
-export HANDLER_PORT=6000
+# export RDS_HOSTNAME=sotg-staging-db.ctifvcvmvtjl.us-west-2.rds.amazonaws.com
+# export HANDLER_ADDRESS=ec2-52-27-78-211.us-west-2.compute.amazonaws.com
+# export API_ADDRESS=sotg-staging.elasticbeanstalk.com
+# export PORT=80
+# export HANDLER_PORT=6000
 
 node streaming/server.js
