c8766732cd6e73758c6354255dd141bbfd15ba6d
Eric Ihli <ericihli@gmail.com>
2015-07-28 10:45:33 -0700

    (refactor) Add console logs.

--- stream.sh
+++ stream.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # export RDS_HOSTNAME=sotg-staging-db.ctifvcvmvtjl.us-west-2.rds.amazonaws.com
-# export HANDLER_ADDRESS=ec2-52-27-78-211.us-west-2.compute.amazonaws.com
+# export HANDLER_ADDRESS=localhost
 # export API_ADDRESS=sotg-staging.elasticbeanstalk.com
-# export PORT=80
+# export API_PORT=80
 # export HANDLER_PORT=6000
 
 node streaming/server.js
