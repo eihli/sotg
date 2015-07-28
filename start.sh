b29c58d7a2b7a06333c144dd42277ae0cb2f55e3
Eric Ihli <ericihli@gmail.com>
2015-07-27 20:45:24 -0700

    (deployment) Change handler environment in circle.yml

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+echo $RDS_HOSTNAME
+echo $RDS_USERNAME
+echo $RDS_PASSWORD
+echo $RDS_DB_NAME
+echo $HANDLER_ADDRESS
+echo $API_ADDRESS
+echo $PORT
+echo $HANDLER_PORT
+
 if [ "$SERVER_TYPE" == "tweethandler" ]
   then
   ./handle.sh
